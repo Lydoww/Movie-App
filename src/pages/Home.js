e6b83ecb31c38a658eabc715825392b024b6ae11
Lydoww <alexis94340@gmail.com>
2024-08-21 18:16:07 +0200

    miss readMe

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header.js";
+import Form from "../components/Form.js";
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <Header />
+      <Form />
+    </div>
+  );
+};
+
+export default Home;
